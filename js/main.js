531e970731815aa8ea12cc5a62122917020e726b
Chfia <37915245+Chfia@users.noreply.github.com>
2023-08-05 23:09:07 +0200

    photos and optimization

--- js/main.js
+++ js/main.js
@@ -1,11 +1,33 @@
-//burger
+let prevScrollPos = window.scrollY;
+
+window.addEventListener("scroll", function () {
+  const currentScrollPos = window.scrollY;
+
+  if (currentScrollPos >= 800) {
+    if (prevScrollPos > currentScrollPos) {
+      document.querySelector(".navtop").style.top = "0";
+    } else {
+      document.querySelector(".navtop").style.top = "-50px";
+    }
+  } else {
+    document.querySelector(".navtop").style.top = "0";
+  }
+
+  prevScrollPos = currentScrollPos;
+});
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
   const burgerContainer = document.querySelector(".burger-container");
+  const navbar = document.querySelector(".navbar");
+  const navbarLinks = document.querySelectorAll(".navbar a");
+  const body = document.querySelector("body");
+  const burgerLines = burgerContainer.querySelectorAll(".burger-line");
 
   burgerContainer.addEventListener("click", function () {
     this.classList.toggle("open");
 
-    const burgerLines = this.querySelectorAll(".burger-line");
     burgerLines.forEach((line, index) => {
       if (this.classList.contains("open")) {
         if (index === 0) {
@@ -20,17 +42,7 @@ document.addEventListener("DOMContentLoaded", function () {
         line.style.opacity = 1;
       }
     });
-  });
-});
 
-//nav
-document.addEventListener("DOMContentLoaded", function() {
-  const burgerContainer = document.querySelector(".burger-container");
-  const navbar = document.querySelector(".navbar");
-  const navbarLinks = document.querySelectorAll(".navbar a");
-  const body = document.querySelector("body");
-
-  burgerContainer.addEventListener("click", function() {
     if (navbar.style.transform === "translateY(-100%)") {
       navbar.style.transform = "translateY(0)";
     } else {
@@ -38,16 +50,15 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   });
 
-  navbarLinks.forEach(function(link) {
-    link.addEventListener("click", function() {
+  navbarLinks.forEach(function (link) {
+    link.addEventListener("click", function () {
       navbar.style.transform = "translateY(-100%)";
     });
   });
 
-  body.addEventListener("click", function(event) {
+  body.addEventListener("click", function (event) {
     if (!navbar.contains(event.target) && navbar.style.transform === "translateY(0)") {
       navbar.style.transform = "translateY(-100%)";
     }
   });
 });
-
