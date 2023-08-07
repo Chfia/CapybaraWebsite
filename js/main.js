1f9c7486201c56cf38a48b589d9ef065de3f5af9
Chfia <37915245+Chfia@users.noreply.github.com>
2023-08-07 22:54:37 +0200

    code fixes

--- js/main.js
+++ js/main.js
@@ -17,7 +17,6 @@ window.addEventListener("scroll", function () {
 });
 
 
-
 document.addEventListener("DOMContentLoaded", function () {
   const burgerContainer = document.querySelector(".burger-container");
   const navbar = document.querySelector(".navbar");
@@ -25,40 +24,41 @@ document.addEventListener("DOMContentLoaded", function () {
   const body = document.querySelector("body");
   const burgerLines = burgerContainer.querySelectorAll(".burger-line");
 
+  let isNavbarClosing = false;
+
   burgerContainer.addEventListener("click", function () {
-    this.classList.toggle("open");
+    burgerContainer.classList.toggle("open");
 
     burgerLines.forEach((line, index) => {
-      if (this.classList.contains("open")) {
-        if (index === 0) {
-          line.style.transform = "translateY(8px) rotate(45deg)";
-        } else if (index === 1) {
-          line.style.opacity = 0;
-        } else if (index === 2) {
-          line.style.transform = "translateY(-8px) rotate(-45deg)";
-        }
-      } else {
-        line.style.transform = "none";
-        line.style.opacity = 1;
-      }
+      line.classList.toggle("open");
     });
 
-    if (navbar.style.transform === "translateY(-100%)") {
+    if (burgerContainer.classList.contains("open")) {
       navbar.style.transform = "translateY(0)";
+      isNavbarClosing = false;
     } else {
       navbar.style.transform = "translateY(-100%)";
+      isNavbarClosing = true;
+    }
+  });
+
+  navbar.addEventListener("transitionend", function () {
+    if (isNavbarClosing) {
+      burgerContainer.classList.remove("open");
     }
   });
 
   navbarLinks.forEach(function (link) {
     link.addEventListener("click", function () {
       navbar.style.transform = "translateY(-100%)";
+      burgerContainer.classList.remove("open");
     });
   });
 
   body.addEventListener("click", function (event) {
     if (!navbar.contains(event.target) && navbar.style.transform === "translateY(0)") {
       navbar.style.transform = "translateY(-100%)";
+      burgerContainer.classList.remove("open");
     }
   });
 });
